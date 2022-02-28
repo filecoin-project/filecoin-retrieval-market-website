42982e101406ca851a3e5745333508d10288e80f
Tiago Moreira <tiagomoreira@nqda.pt>
2022-02-28 17:10:27 +0000

    Add body style

--- src/components/core/global-style/index.tsx
+++ src/components/core/global-style/index.tsx
@@ -3,10 +3,21 @@
  * Module dependencies.
  */
 
+import { color } from '@untile/react-components';
 import { createGlobalStyle } from 'styled-components';
+import { typography } from 'src/styles/type';
 
 /**
  * Export `GlobalStyle` component.
  */
 
-export default createGlobalStyle``;
+export default createGlobalStyle`
+  body {
+    ${typography.styles.p}
+    
+    background: linear-gradient(180deg, #050c1b 0%, #2c61cf 89.85%);
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    color: ${color('textColor')};
+  }
+`;
