3e84b31a726cd6a5e0944e88204e2b8da11ad730
Tiago Moreira <tiagomoreira@nqda.pt>
2022-02-28 17:10:09 +0000

    Add colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -5,7 +5,14 @@
 
 const palette = {
   black: '#000000',
-  white: '#ffffff'
+  blue200: '#75a1ff',
+  blue400: '#2c61cf',
+  blue500: '#2b5fcf',
+  blue600: '#2755b8',
+  cyan: '#00c2ff',
+  dark: '#060b17',
+  white: '#ffffff',
+  yellow: '#fff48c'
 };
 
 /**
@@ -13,5 +20,6 @@ const palette = {
  */
 
 export const colors = {
-  ...palette
+  ...palette,
+  textColor: palette.white
 };
