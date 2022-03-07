a1aca873bc193491abf1a51b8720cedb40436b57
André Moreno <andremoreno3@gmail.com>
2022-03-07 17:51:02 +0000

    Add nodes section to homepage

---
+++ src/components/sections/nodes-section.tsx
@@ -0,0 +1,329 @@
+
+/**
+ * Module dependencies.
+ */
+
+import { Node, defaultNodes } from 'src/core/content-config/nodes-section';
+import { colors } from 'src/styles/colors';
+import { getRandomNumber } from 'src/core/utils/numbers';
+import { transparentize } from '@untile/react-components';
+import React, {
+  ReactElement,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from 'react';
+
+import find from 'lodash/find';
+import forEach from 'lodash/forEach';
+import map from 'lodash/map';
+import pick from 'lodash/pick';
+import styled from 'styled-components';
+
+/**
+ * `Props` type.
+ */
+
+type Props = {
+  className?: string
+};
+
+/**
+ * `Canvas` type.
+ */
+
+type Canvas = {
+  context: CanvasRenderingContext2D,
+  height: number,
+  width: number
+}
+
+/**
+ * Constants.
+ */
+
+const animationFps = 60;
+const floatingDistance = 50;
+const floatingSpeed = 0.5;
+const showNodesIds = false;
+const transitionDuration = 500;
+const web2animationDuration = 5000;
+const web3animationDuration = 5000;
+
+/**
+ * `Wrapper` styled component.
+ */
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
+/**
+ * `drawDot`.
+ */
+
+function drawDot({ context, dotX, dotY, id, radius }) {
+  context.beginPath();
+  context.arc(dotX, dotY, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = colors.blue600;
+  context.fill();
+
+  if (showNodesIds) {
+    context.beginPath();
+    context.font = '20px Arial';
+    context.fillStyle = 'white';
+    context.textAlign = 'center';
+    context.fillText(id, dotX, dotY);
+    context.fill();
+  }
+}
+
+/**
+ * `drawLine`.
+ */
+
+function drawLine({ context, x1, x2, y1, y2 }) {
+  context.beginPath();
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.lineWidth = 1;
+  context.strokeStyle = transparentize(colors.blue600, 0.5);
+  context.stroke();
+}
+
+/**
+ * `clearCanvas`.
+ */
+
+function clearCanvas({ context, height, width }) {
+  context.clearRect(0, 0, width, height);
+  context.fillStyle = 'transparent';
+  context.fillRect(0, 0, width, height);
+}
+
+/**
+ * `animateFloatingDot`.
+ */
+
+function animateFloatingDot({ height, initialX, initialY, node, width }): Node {
+  const { directionX, directionY, dotX, dotY, radius } = node;
+  const limitsX = [initialX - radius - floatingDistance, initialX + radius + floatingDistance];
+  const limitsY = [initialY - radius - floatingDistance, initialY + radius + floatingDistance];
+  const minX = limitsX[0] < 0 ? 0 : limitsX[0];
+  const maxX = limitsX[1] > width ? width : limitsX[1];
+  const minY = limitsY[0] < 0 ? 0 : limitsY[0];
+  const maxY = limitsY[1] > height ? height : limitsY[1];
+  let dirX = directionX;
+  let dirY = directionY;
+
+  if (dotX + radius >= maxX || dotX - radius <= minX) {
+    dirX = directionX < 0 ? getRandomNumber(0, floatingSpeed) : getRandomNumber(-1 * floatingSpeed, 0);
+  }
+
+  if (dotY + radius >= maxY || dotY - radius <= minY) {
+    dirY = directionY < 0 ? getRandomNumber(0, floatingSpeed) : getRandomNumber(-1 * floatingSpeed, 0);
+  }
+
+  return {
+    ...node,
+    directionX: dirX,
+    directionY: dirY,
+    dotX: dotX + dirX,
+    dotY: dotY + dirY
+  };
+}
+
+/**
+ * `NodesSection` component.
+ */
+
+const NodesSection = ({ className }: Props): ReactElement => {
+  const wrapperRef = useRef<HTMLDivElement>();
+  const canvasRef = useRef<HTMLCanvasElement>();
+  const [canvas, setCanvas] = useState<Canvas>();
+  const [connectionsLine, setConnectionsLine] = useState<number>(0);
+  const [hasRenderedFirstTime, setHasRenderedFirstTime] = useState<boolean>(false);
+  const [isShowingWeb3, setShowingWeb3] = useState<boolean>(false);
+  const [isTransitioning, setIsTransitioning] = useState<boolean>(false);
+  const [transitionFade, setTransitionFade] = useState<'increasing' | 'decreasing'>('increasing');
+  const [nodes, setNodes] = useState<Node[]>([]);
+  const initialNodes = useMemo<Node[]>(() => {
+    return map(defaultNodes, node => {
+      return {
+        ...node,
+        directionX: getRandomNumber(-1 * floatingSpeed, floatingSpeed),
+        directionY: getRandomNumber(-1 * floatingSpeed, floatingSpeed),
+        dotX: canvas?.width * 0.05 + canvas?.width * 0.9 * node.dotX / 559,
+        dotY: canvas?.height * 0.05 + canvas?.height * 0.9 * node.dotY / 230
+      };
+    });
+  }, [canvas?.height, canvas?.width]);
+
+  const handleAnimateFloatingDots = useCallback(() => {
+    setNodes(previous => map(previous, (node: Node, index: number) => ({
+      ...animateFloatingDot({
+        ...canvas,
+        initialX: initialNodes[index].dotX,
+        initialY: initialNodes[index].dotY,
+        node
+      })
+    })));
+  }, [canvas, initialNodes]);
+
+  const handleDrawDots = useCallback((nodes: Node[]) => {
+    clearCanvas({ ...canvas });
+    forEach(nodes, (node: Node) => {
+      const { connections, dotX, dotY, laterConnections } = node;
+
+      drawDot({
+        context: canvas.context,
+        ...pick(node, ['dotX', 'dotY', 'id', 'radius'])
+      });
+
+      if (connections) {
+        forEach(connections, connectionId => {
+          const nodeConnected = find(nodes, ({ id }) => id === connectionId);
+
+          drawLine({
+            context: canvas.context,
+            x1: dotX,
+            x2: nodeConnected.dotX,
+            y1: dotY,
+            y2: nodeConnected.dotY
+          });
+        });
+      }
+
+      if (laterConnections) {
+        forEach(laterConnections, connectionId => {
+          const nodeConnected = find(nodes, ({ id }) => id === connectionId);
+
+          drawLine({
+            context: canvas.context,
+            x1: dotX,
+            x2: dotX + (nodeConnected.dotX - dotX) * connectionsLine,
+            y1: dotY,
+            y2: dotY + (nodeConnected.dotY - dotY) * connectionsLine
+          });
+        });
+      }
+    });
+  }, [canvas, connectionsLine]);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      if (wrapperRef && wrapperRef?.current && canvasRef && canvasRef?.current) {
+        const canvasWidth = wrapperRef.current.clientWidth;
+        const canvasHeight = canvasWidth * 0.412;
+
+        setHasRenderedFirstTime(false);
+        setCanvas({
+          context: canvasRef?.current?.getContext('2d'),
+          height: canvasHeight,
+          width: wrapperRef.current.clientWidth
+        });
+      }
+    };
+
+    handleWindowResize();
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+
+  useEffect(() => {
+    if (canvas && !hasRenderedFirstTime) {
+      handleDrawDots(initialNodes);
+      setNodes(initialNodes);
+      setHasRenderedFirstTime(true);
+    }
+  }, [canvas, handleDrawDots, hasRenderedFirstTime, initialNodes]);
+
+  useEffect(() => {
+    if (canvas && hasRenderedFirstTime) {
+      const timeout = setTimeout(() => {
+        setConnectionsLine(transitionFade === 'increasing' ? 0 : 1);
+        setShowingWeb3(previous => !previous);
+        setIsTransitioning(true);
+      }, isShowingWeb3 ? web3animationDuration : web2animationDuration);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [canvas, hasRenderedFirstTime, isShowingWeb3, transitionFade]);
+
+  useEffect(() => {
+    if (canvas && hasRenderedFirstTime) {
+      const interval = setInterval(() => {
+        handleAnimateFloatingDots();
+      }, 1000 / animationFps);
+
+      return () => clearInterval(interval);
+    }
+  }, [canvas, handleAnimateFloatingDots, hasRenderedFirstTime]);
+
+  useEffect(() => {
+    if (canvas && hasRenderedFirstTime && isTransitioning) {
+      const multiplier = transitionDuration / animationFps;
+      const lineSizeMultiplier = 1 / animationFps;
+      const timeout = setTimeout(() => {
+        setIsTransitioning(false);
+
+        if (transitionFade === 'increasing') {
+          setTransitionFade('decreasing');
+
+          return;
+        }
+
+        setTransitionFade('increasing');
+      }, transitionDuration);
+
+      const interval = setInterval(() => {
+        setConnectionsLine(previous => {
+          if (transitionFade === 'increasing') {
+            return previous + lineSizeMultiplier > 1 ? 1 : previous + lineSizeMultiplier;
+          }
+
+          return previous - lineSizeMultiplier < 0 ? 0 : previous - lineSizeMultiplier;
+        });
+      }, multiplier);
+
+      return () => {
+        clearInterval(interval);
+        clearTimeout(timeout);
+      };
+    }
+  }, [
+    canvas,
+    hasRenderedFirstTime,
+    isTransitioning,
+    transitionFade
+  ]);
+
+  useEffect(() => {
+    if (canvas && hasRenderedFirstTime) {
+      handleDrawDots(nodes);
+    }
+  }, [canvas, handleDrawDots, hasRenderedFirstTime, isShowingWeb3, nodes]);
+
+  return (
+    <Wrapper
+      className={className}
+      ref={wrapperRef}
+    >
+      <canvas
+        height={canvas?.height}
+        ref={canvasRef}
+        width={canvas?.width}
+      />
+    </Wrapper>
+  );
+};
+
+/**
+ * Export `NodesSection` component.
+ */
+
+export default NodesSection;
---
+++ src/core/content-config/nodes-section.ts
@@ -0,0 +1,489 @@
+
+/**
+ * Constants.
+ */
+
+const bigRadius = 10;
+const smallRadius = 5;
+
+/**
+ * Export `Node` type.
+ */
+
+export type Node = {
+  connections?: string[],
+  directionX?: number,
+  directionY?: number,
+  dotX: number,
+  dotY: number,
+  id: string,
+  laterConnections?: string[],
+  radius: number,
+  type: 'node' | 'subnode'
+}
+
+/**
+ * Export `defaultNodes`.
+ */
+
+export const defaultNodes: Node[] = [{
+  dotX: 27,
+  dotY: 184,
+  id: '1.0',
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['1.0'],
+  dotX: 18,
+  dotY: 155,
+  id: '1.1',
+  laterConnections: ['2.1', '3.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['1.0'],
+  dotX: 0,
+  dotY: 185,
+  id: '1.2',
+  laterConnections: ['1.1', '1.3', '2.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['1.0'],
+  dotX: 9,
+  dotY: 225,
+  id: '1.3',
+  laterConnections: ['3.4'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['3.0'],
+  dotX: 57,
+  dotY: 77,
+  id: '2.0',
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['2.0'],
+  dotX: 2,
+  dotY: 84,
+  id: '2.1',
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['2.0'],
+  dotX: 21,
+  dotY: 52,
+  id: '2.2',
+  laterConnections: ['2.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['2.0'],
+  dotX: 82,
+  dotY: 2,
+  id: '2.3',
+  laterConnections: ['2.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['2.0'],
+  dotX: 141,
+  dotY: 43,
+  id: '2.4',
+  laterConnections: ['2.3', '5.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['1.0', '4.0'],
+  dotX: 91,
+  dotY: 148,
+  id: '3.0',
+  laterConnections: ['2.0'],
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['3.0'],
+  dotX: 64,
+  dotY: 128,
+  id: '3.1',
+  laterConnections: ['2.0', '2.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['3.0'],
+  dotX: 84,
+  dotY: 103,
+  id: '3.2',
+  laterConnections: ['2.0', '5.0', '5.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['3.0'],
+  dotX: 115,
+  dotY: 171,
+  id: '3.3',
+  laterConnections: ['3.4', '4.2', '4.3'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['3.0'],
+  dotX: 95,
+  dotY: 197,
+  id: '3.4',
+  laterConnections: ['4.3'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  dotX: 209,
+  dotY: 159,
+  id: '4.0',
+  laterConnections: ['6.0'],
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['4.0'],
+  dotX: 188,
+  dotY: 138,
+  id: '4.1',
+  laterConnections: ['5.0', '6.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['4.0'],
+  dotX: 154,
+  dotY: 170,
+  id: '4.2',
+  laterConnections: ['4.3'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['4.0'],
+  dotX: 214,
+  dotY: 221,
+  id: '4.3',
+  laterConnections: ['4.4'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['4.0'],
+  dotX: 254,
+  dotY: 167,
+  id: '4.4',
+  laterConnections: ['8.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['4.0'],
+  dotX: 147,
+  dotY: 121,
+  id: '5.0',
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['5.0'],
+  dotX: 119,
+  dotY: 135,
+  id: '5.1',
+  laterConnections: ['3.0'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['5.0'],
+  dotX: 145,
+  dotY: 78,
+  id: '5.2',
+  laterConnections: ['6.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  dotX: 277,
+  dotY: 105,
+  id: '6.0',
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['6.0'],
+  dotX: 218,
+  dotY: 115,
+  id: '6.1',
+  laterConnections: ['5.0', '6.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['6.0'],
+  dotX: 226,
+  dotY: 74,
+  id: '6.2',
+  laterConnections: ['7.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['6.0'],
+  dotX: 296,
+  dotY: 58,
+  id: '7.0',
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['7.0'],
+  dotX: 241,
+  dotY: 0,
+  id: '7.1',
+  laterConnections: ['2.4'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['7.0'],
+  dotX: 309,
+  dotY: 2,
+  id: '7.2',
+  laterConnections: ['7.1', '7.3', '10.3'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['7.0'],
+  dotX: 325,
+  dotY: 43,
+  id: '7.3',
+  laterConnections: ['10.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['6.0'],
+  dotX: 384,
+  dotY: 161,
+  id: '8.0',
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['8.0'],
+  dotX: 327,
+  dotY: 174,
+  id: '8.1',
+  laterConnections: ['4.3', '4.4', '9.5'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['8.0'],
+  dotX: 294,
+  dotY: 146,
+  id: '8.2',
+  laterConnections: ['6.0'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['8.0'],
+  dotX: 340,
+  dotY: 123,
+  id: '8.3',
+  laterConnections: ['8.4', '10.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['8.0'],
+  dotX: 379,
+  dotY: 90,
+  id: '8.4',
+  laterConnections: ['10.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['8.0'],
+  dotX: 398,
+  dotY: 111,
+  id: '8.5',
+  laterConnections: ['8.4', '9.2', '12.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['11.0'],
+  dotX: 457,
+  dotY: 199,
+  id: '9.0',
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['8.0', '9.0'],
+  dotX: 416,
+  dotY: 197,
+  id: '9.1',
+  laterConnections: ['9.5'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['9.0'],
+  dotX: 437,
+  dotY: 174,
+  id: '9.2',
+  laterConnections: ['12.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['9.0'],
+  dotX: 482,
+  dotY: 205,
+  id: '9.3',
+  laterConnections: ['13.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['9.0'],
+  dotX: 489,
+  dotY: 230,
+  id: '9.4',
+  laterConnections: ['9.3', '9.5', '13.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['9.0'],
+  dotX: 445,
+  dotY: 230,
+  id: '9.5',
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['12.0'],
+  dotX: 458,
+  dotY: 33,
+  id: '10.0',
+  laterConnections: ['12.3', '14.1'],
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['10.0'],
+  dotX: 399,
+  dotY: 78,
+  id: '10.1',
+  laterConnections: ['10.2', '12.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['10.0'],
+  dotX: 325,
+  dotY: 76,
+  id: '10.2',
+  laterConnections: ['10.3'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['10.0'],
+  dotX: 377,
+  dotY: 33,
+  id: '10.3',
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['10.0'],
+  dotX: 531,
+  dotY: 10,
+  id: '10.4',
+  laterConnections: ['14.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['9.0', '12.0'],
+  dotX: 496,
+  dotY: 152,
+  id: '11.0',
+  laterConnections: ['13.3'],
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['11.0'],
+  dotX: 468,
+  dotY: 157,
+  id: '11.1',
+  laterConnections: ['12.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  dotX: 506,
+  dotY: 110,
+  id: '12.0',
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['12.0'],
+  dotX: 445,
+  dotY: 142,
+  id: '12.1',
+  laterConnections: ['12.2'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['12.0'],
+  dotX: 433,
+  dotY: 116,
+  id: '12.2',
+  laterConnections: ['12.3'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['12.0'],
+  dotX: 464,
+  dotY: 97,
+  id: '12.3',
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['12.0'],
+  dotX: 543,
+  dotY: 182,
+  id: '13.0',
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['13.0'],
+  dotX: 558,
+  dotY: 197,
+  id: '13.1',
+  laterConnections: ['13.2', '13.4'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['13.0'],
+  dotX: 491,
+  dotY: 199,
+  id: '13.2',
+  laterConnections: ['13.3'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['13.0'],
+  dotX: 508,
+  dotY: 160,
+  id: '13.3',
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['13.0'],
+  dotX: 548,
+  dotY: 138,
+  id: '13.4',
+  laterConnections: ['14.0'],
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['12.0'],
+  dotX: 550,
+  dotY: 89,
+  id: '14.0',
+  radius: bigRadius,
+  type: 'node'
+}, {
+  connections: ['14.0'],
+  dotX: 517,
+  dotY: 76,
+  id: '14.1',
+  radius: smallRadius,
+  type: 'subnode'
+}, {
+  connections: ['14.0'],
+  dotX: 559,
+  dotY: 62,
+  id: '14.2',
+  laterConnections: ['14.1'],
+  radius: smallRadius,
+  type: 'subnode'
+}];
