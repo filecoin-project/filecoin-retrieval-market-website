e89c220cecdc09dc9853d408d601afebf7da5fb3
Tiago Moreira <tiagomoreira@nqda.pt>
2022-03-15 17:03:14 +0000

    Add FadeInUp

---
+++ src/components/core/animations/fade-in-up.tsx
@@ -0,0 +1,83 @@
+
+/**
+ * Module dependencies.
+ */
+
+import { IntersectionOptions, useInView } from 'react-intersection-observer';
+import { ifProp, prop, theme } from 'styled-tools';
+import React, { ElementType, ReactElement, ReactNode } from 'react';
+import merge from 'lodash/merge';
+import omit from 'lodash/omit';
+import styled, { css } from 'styled-components';
+
+/**
+ * `Props` type.
+ */
+
+type Props = {
+  as?: ElementType,
+  children: ReactNode,
+  className?: string,
+  options?: IntersectionOptions & {
+    distance?: any,
+    transitionDelay?: number,
+    transitionTime?: number
+  }
+};
+
+/**
+ * `Content` styled component.
+ */
+
+const Content = styled.div<{
+  distance?: string,
+  isVisible: boolean,
+  transitionDelay?: number,
+  transitionTime?: number
+}>`
+  transition:
+    opacity ${theme('animations.defaultTransition')} ${prop('transitionDelay', 0)}s,
+    transform ${prop('transitionTime', 0.5)}s cubic-bezier(0, 0.59, 0, 1) ${prop('transitionDelay', 0)}s;
+  will-change: transform;
+
+  ${ifProp('isVisible', css`
+    opacity: 1;
+    transform: none;
+  `, css`
+    opacity: 0;
+    transform: translateY(${prop('distance', '20%')});
+  `)}
+`;
+
+/**
+ * `FadeInUpAnimation` component.
+ */
+
+const FadeInUpAnimation = (props: Props): ReactElement => {
+  const { children, className, options, ...rest } = props;
+  const inViewOptions = omit(options, ['distance, transitionDelay, transitionTime']);
+  const [ref, inView] = useInView(merge({}, {
+    threshold: 0.1,
+    triggerOnce: true
+  }, inViewOptions));
+
+  return (
+    <Content
+      {...rest}
+      className={className}
+      distance={options?.distance}
+      isVisible={inView}
+      ref={ref}
+      transitionDelay={options?.transitionDelay}
+      transitionTime={options?.transitionTime}
+    >
+      {children}
+    </Content>
+  );
+};
+
+/**
+ * Export `FadeInUpAnimation` component.
+ */
+
+export default FadeInUpAnimation;
