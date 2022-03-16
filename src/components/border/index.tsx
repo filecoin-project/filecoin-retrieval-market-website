15b3659f341ca460c2738b6412554084aa692afe
Tiago Moreira <tiagomoreira@nqda.pt>
2022-03-16 17:41:45 +0000

    Add Border component

---
+++ src/components/border/index.tsx
@@ -0,0 +1,23 @@
+
+/**
+ * Modules dependencies.
+ */
+
+import { color } from '@untile/react-components';
+import styled from 'styled-components';
+
+/**
+ * `Border` styled component.
+ */
+
+const Border = styled.div`
+  background-color: ${color('white')};
+  height: 1px;
+  width: 100%;
+`;
+
+/**
+ * Export `Border` component.
+ */
+
+export default Border;
