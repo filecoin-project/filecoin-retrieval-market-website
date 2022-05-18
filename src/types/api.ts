f06a675c61fc19a8643f0ba0b71fa6ea0fbbb9d2
Tiago Moreira <tiagomoreira@nqda.pt>
2022-05-18 15:01:54 +0100

    Update SettingProps type

--- src/types/api.ts
+++ src/types/api.ts
@@ -51,7 +51,7 @@ export interface RoadmapProps {
 
 export interface SettingProps {
   value: string;
-  type: 'email' | 'social-network' | 'footer-link';
+  type: 'cookies-button-link' | 'email' | 'social-network' | 'footer-link';
   name: string;
 }
 
